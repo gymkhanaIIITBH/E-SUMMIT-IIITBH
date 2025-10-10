589419e63cc067b62d11c2827d42653c1a4e3cd5
Yug00007 <yugsharma1047@gmail.com>
2025-10-11 03:27:27 +0530

    wrapped up about section

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -4,6 +4,7 @@ function AboutSection() {
   return (
     <>
         <div className="flex flex-col items-center p-10 h-80 w-full bg-white">
+            <div className="h-24 w-[3px] bg-gray-800/30 mb-1" />
             <h1 className="text-[#07617d] text-sm font-bold font-montserrat"> ABOUT US</h1>
             <p className="px-40 py-5 text-xl font-semibold font-newsletter text-gray-700"> <span className="text-[#07617d]">E-Cell, IIIT Bhagalpur</span>  is dedicated to nurturing an entrepreneurial spirit among students and igniting their drive for innovation. As we host our first-ever E-Summit, our mission is to cultivate ideas at their earliest stage and empower them to evolve into impactful ventures. Through collaborations with incubators, mentors, and investors, we aim to bridge the gap between visionary concepts and successful start-ups — fostering a vibrant entrepreneurial ecosystem within our campus. </p>
         </div>
